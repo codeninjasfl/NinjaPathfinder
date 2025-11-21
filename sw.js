8ee75be515c0f2eb702f409a67ee01caf1a3b700
codeninjasfl <brennan.shea@codeninjas.com>
2025-11-20 21:23:46 -0500

    Organizational Fix

--- sw.js
+++ sw.js
@@ -1,43 +1,20 @@
-const CACHE_NAME = 'ninja-pathfinder-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-  // Add other CSS or JS files here if you have them, e.g., './style.css'
+const CACHE_NAME = 'ninja-path-finder-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './index.css',
+    './manifest.json',
+    './icon-192.png'
 ];
 
-// Install Event: Cache core assets
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching all: Ninja Pathfinder assets');
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
-// Fetch Event: Serve from cache first, fall back to network
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Activate Event: Clean up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
 });
